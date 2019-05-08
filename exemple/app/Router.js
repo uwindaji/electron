1d0c3677dfbe1aca72d7655e096ce6ab13d1b774
Lakhdar Rouibah <marouane@imac-de-lakhdar.home>
2019-05-08 10:25:50 +0200

    my first commit

---
+++ exemple/app/Router.js
@@ -0,0 +1,19 @@
+class Router {
+
+    static loader ($page) {
+        
+        $("#container" ).load( "app/"+ $page +"/"+ $page +".html", ()=>{
+            $.getScript("ressources/js/jquery-3.4.1.min.js", ()=>{
+                $.getScript("ressources/js/popper.min.js", ()=>{
+                    $.getScript("ressources/js/bootstrap.min.js", ()=>{
+                        $.getScript("app/"+ $page +"/"+ $page +".js");
+                    });
+                });
+            });
+        });
+
+    }
+
+}
+
+Router.loader("index");
